09ccf078212be88520272deb58631e3d9b35de7b
Malek <johnmalek54@gmail.com>
2022-08-23 06:47:13 -0700

    Rest of the tasks

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        last_digit = (number % 10) - 10
+        print(last_digit)
+    else:
+        last_digit = number % 10
+        print(last_digit)
