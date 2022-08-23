ec140c20af7c39e8b3a7ab60a3747f84bb488f85
Malek <johnmalek54@gmail.com>
2022-08-23 04:51:07 -0700

    First 8 projects

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for n in range(0, 10):
+    for m in range(0, 10):
+        if n < m:
+            if int(str(n) + str(m)) < 89:
+                print("{}{}".format(n, m), end=", ")
+            else:
+                print("{}{}".format(n,m), end="\n")
+        
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def islower(c):
+    if ord(c) in range(97, 123):
+        return True
+    else:
+        return False
+
+print("a is {}".format("lower" if islower("a") else "upper"))
+print("H is {}".format("lower" if islower("H") else "upper"))
+print("A is {}".format("lower" if islower("A") else "upper"))
+print("3 is {}".format("lower" if islower("3") else "upper"))
+print("g is {}".format("lower" if islower("g") else "upper"))
