96e4e3d12679d5d5cbd53f8cb2122c491ed9f811
Malek <johnmalek54@gmail.com>
2022-08-25 09:33:25 -0700

    Task 5

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import hidden_4
+    all_names = dir(hidden_4)
+    print(all_names)
+    
---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+if __name__ == "main":
+    from variable_load_5 import a
+    print(a)
