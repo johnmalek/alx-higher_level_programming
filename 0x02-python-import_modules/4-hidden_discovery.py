e6c0c6c5c7ded1abf084513c6ff42b55ea76c2b6
Malek <johnmalek54@gmail.com>
2022-08-25 13:08:05 -0700

    More corrections

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,8 +3,7 @@ import sys
 import hidden_4 as hidden
 
 if __name__ != "__main__":
-        exit()
-
-        for name in dir(hidden):
-                if name[0:2] != "__":
-                            print(name)
+    exit()
+    for name in dir(hidden):
+        if name[0:2] != "__":
+            print(name)
