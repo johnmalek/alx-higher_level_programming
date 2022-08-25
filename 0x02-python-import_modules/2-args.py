13932305671f6076dd932c448853bf42b456fba8
Malek <johnmalek54@gmail.com>
2022-08-25 03:09:11 -0700

    Another correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 
-import sys
-arguments = sys.argv
-number_of_arguments = len(arguments) - 1
-if number_of_arguments == 1:
-    print(f"{number_of_arguments} argument.")
-elif number_of_arguments == 0:
-    print(f"{number_of_arguments} arguments.")
-else:
-    print(f"{number_of_arguments} arguments:")
+if __name__ == "__main__":
+    import sys
+    arguments = sys.argv
+    number_of_arguments = len(arguments) - 1
+    if number_of_arguments == 1:
+        print(f"{number_of_arguments} argument.")
+    elif number_of_arguments == 0:
+        print(f"{number_of_arguments} arguments.")
+    else:
+        print(f"{number_of_arguments} arguments:")
 
-for arg in range(len(arguments)):
-    if arg > 0:
-        print("{}: {}".format(arg, arguments[arg]))
+    for arg in range(len(arguments)):
+        if arg > 0:
+            print("{}: {}".format(arg, arguments[arg]))
