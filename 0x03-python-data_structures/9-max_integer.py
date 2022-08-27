632694d92d1d2ffb6932cc0a7d6fbbe493acb2a5
Malek <johnmalek54@gmail.com>
2022-08-27 02:51:14 -0700

    Task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    """
+    Finds the biggest integer of a list
+    """
+    biggest = 0
+    if len(my_list) == 0:
+        return
+    for item in my_list:
+        if item > biggest:
+            biggest = item
+    return biggest
