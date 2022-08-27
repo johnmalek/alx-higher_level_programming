8aab06ccc605f661954a0daea9eccc5dd8a88ae7
Malek <johnmalek54@gmail.com>
2022-08-27 02:09:39 -0700

    Corrections

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -6,4 +6,7 @@ def print_reversed_list_integer(my_list=[]):
     Prints all integers of a list, in reverse order
     """
 
-    print("{}".format(my_list[::-1]))
+    if type(my_list) is list:
+        my_list.reverse()
+        for i in my_list:
+            print("{:d}".format(i))
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -8,10 +8,10 @@ def new_in_list(my_list, idx, element):
     """
 
     if idx < 0:
-        return my_list
+        return (my_list)
     elif idx > len(my_list):
-        return my_list
+        return (my_list)
     else:
         original_list = my_list.copy()
         original_list[idx] = element
-        return original_list
+        return (original_list)
