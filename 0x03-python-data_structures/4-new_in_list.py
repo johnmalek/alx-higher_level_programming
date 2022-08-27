7e357a654181c9f3287df1abb90f9706e2866c48
Malek <johnmalek54@gmail.com>
2022-08-27 02:31:46 -0700

    Corrections

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -7,11 +7,9 @@ def new_in_list(my_list, idx, element):
     position without modifying the original list
     """
 
-    if idx < 0:
+    if idx < 0 or idx >= len(my_list):
         return (my_list)
-    elif idx > len(my_list):
-        return (my_list)
-    else:
-        original_list = my_list.copy()
-        original_list[idx] = element
-        return (original_list)
+    
+    original_list = my_list.copy()
+    original_list[idx] = element
+    return (original_list)
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,14 +3,14 @@
 def print_matrix_integer(matrix=[[]]):
 
     """
-    Prints a matrix
+    Prints a matrix of integers
     """
 
     for row in matrix:
         row_length = len(row)
         for i in range(row_length):
             if i != row_length - 1:
-                print("{}".format(row[i]), end=" ")
+                print("{:d}".format(row[i]), end=" ")
             else:
-                print("{}".format(row[i]), end=" ")
+                print("{:d}".format(row[i]), end=" ")
         print()
