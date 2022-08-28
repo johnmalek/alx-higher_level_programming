6abbbac6c979930afa5321e3407cc02b2a3d0cfa
Malek <johnmalek54@gmail.com>
2022-08-28 08:59:11 -0700

    Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,10 +4,9 @@ def multiple_returns(sentence):
     """
     Returns a tuple with the length of
     a string and its first character
-    """ 
+    """
     if len(sentence) == 0:
         first = None
     else:
         first = sentence[0]
-
     return (len(sentence), first)
