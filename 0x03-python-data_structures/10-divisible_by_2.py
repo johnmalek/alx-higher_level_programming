067a5757ecd116c7cc57bddede6299411779154d
Malek <johnmalek54@gmail.com>
2022-08-28 04:11:48 -0700

    Two more tasks

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    
+    """
+    Finds all multiples of 2 in a list
+    """
+
+    for item in my_list:
+        if item % 2 == 0:
+            return my_list
+        else:
+            return my_list
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
+
     """
     Finds the biggest integer of a list
     """
+
     biggest = 0
     if len(my_list) == 0:
         return
