09daec8dfc606588b17dfd940f25d092ff961dd0
Malek <johnmalek54@gmail.com>
2022-09-12 14:00:00 -0700

    Corrections on some mandatory tasks

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end='')
             count += 1
-        except:
+        except Exception as err:
             continue
     print('')
     return count
