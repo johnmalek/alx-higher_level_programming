6e8e18c32da464b9ecc2f277d2a44c495c450840
Malek <johnmalek54@gmail.com>
2022-09-26 04:26:28 -0700

    Task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Defines a class
+"""
+
+
+class MyList(list):
+    """
+    A derived class that inherits from base class list
+    """
+    
+    def print_sorted(self):
+        """
+        Prints the list in ascending order
+        """
+        self_copy = self[:]
+        self_copy.sort()
+        print(self_copy)
