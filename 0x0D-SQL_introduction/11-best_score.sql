b0d931df8c86924d959926f24daf2f2c922eae36
Malek <johnmalek54@gmail.com>
2022-11-01 11:32:48 -0700

    Task 11 correction

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- Displays records using SELECT
 -- Displays records in second_table with score >= 10
-SELECT score, name FROM second_table WHERE score >= 10;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
