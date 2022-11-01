256a6961386bd3e7c42cdb80bfa46099af98c3aa
Malek <johnmalek54@gmail.com>
2022-11-01 11:36:22 -0700

    Task 9 correction

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,16 @@
 -- Creates a new table using CREATE
 -- Creates a table second_table
 CREATE TABLE if NOT EXISTS second_table (id INT, name VARCHAR, score INT);
+-- query to insert first row into the table
+INSERT INTO second_table (id, name, score)
+VALUES (1, 'John', 10);
+-- Query to insert second row into the table
+INSERT INTO second_table (id, name, score)
+VALUES (2, 'Alex', 3);
+-- Quert to insert third row into the table
+INSERT INTO second_table (id, name, score)
+VALUES (3, 'Bob', 14);
+-- Query to insert fourth row into the table
+INSERT INTO second_table (id, name, score)
+VALUES (4, 'George', 8);
 
