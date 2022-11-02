3dd8ca91c5525cbc94b3c9be4db650f86426eb7e
Malek <johnmalek54@gmail.com>
2022-11-02 11:26:35 -0700

    Task 2

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- Display priviledges using SHOW GRANTS
 -- List user_0d_1 and user_0d_2 priviledges
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@loclahost;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'loclahost';
 
