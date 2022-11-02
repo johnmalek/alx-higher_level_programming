3b94fad368a4777150d6f53e373a81eb57002a4d
Malek <johnmalek54@gmail.com>
2022-11-02 12:40:45 -0700

    Corrections

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Create user using CREATE
 -- Create user_0d_1
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user-0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
 
--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- Display records using SELECT
 -- List all cities of California
-SELECT (id, name) FROM cities WHERE state_id = (SELECT id FROM states WHERE name="California") ORDER BY cities.id ASC;
+SELECT (id, name) FROM cities WHERE state_id = (SELECT id FROM states WHERE name='California') ORDER BY cities.id ASC;
 
