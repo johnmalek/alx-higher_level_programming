62054788ac0c2bdb201fdbf961139b4d6f1ba44e
Malek <johnmalek54@gmail.com>
2022-11-28 04:44:23 -0800

    Task 3 and 4 corrections

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-if (isNaN(args[0])) {
+if (args[0]) {
+  console.log('My number: ' + parseInt(args[0]));
+} else {
   console.log('Not a number');
-}else {
-  console.log('My number: ', parseInt(args[0]));
 }
