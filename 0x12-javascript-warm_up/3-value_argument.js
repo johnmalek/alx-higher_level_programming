743b7b8049c12e726547c02f5e32d544dc4aa2dd
Malek <johnmalek54@gmail.com>
2022-11-28 04:37:24 -0800

    Task 3 corrections

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv;
-if (args.length < 3) {
-  console.log('No argument');
-} else {
+if (args[2]) {
   console.log(args[2]);
+} else {
+  console.log('No argument');
 }
