7f92d278a9730c13f72875745dd279ca946d3f02
Malek <johnmalek54@gmail.com>
2022-12-15 12:05:33 -0800

    All files

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Script that distributes an
+archive to your web servers, using the function do_deploy.
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    Return the peak in a list of integers
+    """
+    if len(list_of_integers) > 0:
+        list_of_integers.sort()
+        return list_of_integers[-1]
+    else:
+        return None
