377736fb97998e444f2c88c2a64ef29cbd6c5b4a
Malek <johnmalek54@gmail.com>
2022-12-19 03:03:30 -0800

    Correction

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+A Script that fetches a url
+"""
 import urllib.request
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html = response.read()
