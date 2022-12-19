11e7a75248608a141f3c93faeb18f89cf03d0e8e
Malek <johnmalek54@gmail.com>
2022-12-19 11:24:51 -0800

    Advanced task

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes 2 arguments in order to solve this challenge.
+"""
+from requests import get
+from sys import argv
+
+
+if __name__ == "__main__":
+    username = argv[2]
+    repo = argv[1]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(username, repo)
+    response = get(url)
+    try:
+        objects = response.json()
+        for i, obj in enumerate(objects):
+            print('{}: {}'.format(obj.get('sha'),
+                                  obj.get('commit').get('author')
+                                  .get('name')))
+            if i == 9:
+                break
+    except ValueError:
+        pass
