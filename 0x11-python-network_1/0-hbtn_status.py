e3f9ec69f2ffe0257135e2f2017bed6b377f8a1a
Malek <johnmalek54@gmail.com>
2022-12-19 02:57:04 -0800

    Task 0 + README

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+import urllib.request
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    html = response.read()
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,1 @@
+Python Netwok 1
