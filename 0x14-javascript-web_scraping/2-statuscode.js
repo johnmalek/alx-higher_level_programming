16eb1eb1491cc62d2207dea38970986f483800fa
Malek <johnmalek54@gmail.com>
2023-01-17 11:41:40 -0800

    Rest of the mandatory tasks

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,6 +6,6 @@ request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    console.log('code: ', response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
